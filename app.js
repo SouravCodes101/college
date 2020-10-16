d9b5df3df4b0e39e01ace14e80e55408ec4c1880
unknown <saurabc987@gmail.comgit config -- global user.name Sourav>
2020-10-16 13:29:05 +0530

    Add Environment Files

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require ("ejs");
 const mongoose = require ("mongoose");
+const encrypt = require ("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -13,9 +17,23 @@ extended: true
 
 mongoose.connect("mongodb://localhost:27017/noticeDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    roll: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+
 app.get("/", function(req, res){
     res.render("home");
 });
+
 
 app.get("/login", function(req, res){
     res.render("login");
@@ -27,6 +45,41 @@ app.get("/user", function(req, res){
 
 app.get("/split", function(req, res) {
     res.render("split");
+});
+
+app.post("/", function(req, res) {
+    const newUser = new User ({
+        name: req.body.name,
+        email: req.body.username,
+        roll: req.body.roll,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+
+app.post("/login", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets")
+                }
+            }
+        }
+    });
 });
 
 
